a8a26d25d8557fef28616bd6f24cf1e9e872496b
pejman1991 <pejmanrezaee70@yahoo.com>
2025-01-09 10:01:08 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+﻿// Contact Form Submission
+const contactForm = document.getElementById('contact-form');
+contactForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  alert('پیام شما ارسال شد!');
+});
+
+// Login Form Submission
+const loginForm = document.getElementById('login-form');
+loginForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  if (username === 'admin' && password === '1234') {
+    alert('ورود موفقیت‌آمیز!');
+  } else {
+    alert('نام کاربری یا رمز عبور اشتباه است.');
+  }
+});
